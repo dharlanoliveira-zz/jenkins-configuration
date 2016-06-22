13a169b6a30a7ae251f3c995998dc3d735b2adfb
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-22 16:53:44 -0300

    * Externalizando configuração do projeto

--- montar_projetos.groovy
+++ montar_projetos.groovy
@@ -3,8 +3,10 @@ import groovy.json.JsonSlurper
 println "#######################################"
 println new File(".").getAbsolutePath()
 
+def file = new URL('https://raw.githubusercontent.com/dharlanoliveira/jenkins-configuration/master/projetos.json').openStream()
+
 def jsonSlurper = new JsonSlurper()
-def conf = jsonSlurper.parseText(new File('./projetos.json').getText('UTF-8'))
+def conf = jsonSlurper.parse(file)
 
 conf.each {
     println "Montando projeto " + it.id
