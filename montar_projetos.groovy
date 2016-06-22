5b9b4ea988fd8708c6365fb100562efbb73651a9
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-22 15:40:31 -0300

    * Evoluindo

--- montar_projetos.groovy
+++ montar_projetos.groovy
@@ -1,9 +1,21 @@
-pipelineJob("jobjob") {
+println "To dentro ######################"
+
+pipelineJob("spring-boot-sample") {
+    displayName('Build de exemplo do spring boot sample')
+
     logRotator {
         numToKeep 10
     }
 
     triggers {
         scm 'H/5 * * * *'
+    }
+
+     definition {
+        cpsScm {
+            scm {
+                git('https://github.com/dharlanoliveira/spring-boot-rest-example.git')
+            }
+        }
     }
 }
