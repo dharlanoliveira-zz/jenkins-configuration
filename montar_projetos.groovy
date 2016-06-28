adc205ce4363d14f857d80d4358dfa20b36c6ed6
Dharlan Oliveira <dharlanoliveira@gmail.com>
2016-06-28 20:39:27 -0300

    * Externalizando configuração do projeto

--- montar_projetos.groovy
+++ montar_projetos.groovy
@@ -1,14 +1,35 @@
 import groovy.json.JsonSlurper
 
-def file = new URL('https://raw.githubusercontent.com/dharlanoliveira/jenkins-configuration/master/projetos.json').openStream()
-
-def jsonSlurper = new JsonSlurper()
-def conf = jsonSlurper.parse(file)
+def conf = carregarConfiguracaoArquivo('https://raw.githubusercontent.com/dharlanoliveira/jenkins-configuration/master/projetos.json');
 
 conf.each {
     def obj = it;
     println "Montando projeto " + obj.id
     criarPasta(obj.folder)
+    criarProjetoPipeline(obj);
+}
+
+def carregarConfiguracaoArquivo(String caminho){
+    def file = new URL(caminho).openStream()
+
+    def jsonSlurper = new JsonSlurper()
+    return jsonSlurper.parse(file)
+}
+
+def criarPasta (String pasta) {
+    if(pasta.isEmpty()){
+        return;
+    }
+    String[] folders = pasta.split("/")
+    String caminhoPasta = "";
+    for(int i = 0 ; i < folders.length; ++i){
+        caminhoPasta = caminhoPasta.concat(folders[i] + "/")
+        println "Criando pasta " + caminhoPasta
+        folder(caminhoPasta.substring(0,caminhoPasta.length() - 1));
+    }
+}
+
+def criarProjetoPipeline (obj){
     pipelineJob(obj.folder + "/" + obj.id) {
         displayName(obj.nome)
         description("Job do projeto " + obj.nome);
@@ -38,17 +59,3 @@ conf.each {
         }
     }
 }
-
-void criarPasta (String pasta) {
-    if(pasta.isEmpty()){
-        return;
-    }
-    String[] folders = pasta.split("/")
-    String caminhoPasta = "";
-    for(int i = 0 ; i < folders.length; ++i){
-        caminhoPasta = caminhoPasta.concat(folders[i] + "/")
-        println "Criando pasta " + caminhoPasta
-        folder(caminhoPasta.substring(0,caminhoPasta.length() - 1));
-    }
-}
-
